b84eb6db97d5bbec5de9ff0805338c0fef56e23f
madhushreebagchi <59157179+madhushreebagchi@users.noreply.github.com>
2019-12-31 03:07:26 -0800

    Add files via upload

--- Newyearparty.java
+++ Newyearparty.java
@@ -13,18 +13,26 @@ class Newyear
 	int eligibleAge=16;
 	Scanner sc=new Scanner(System.in);
 	int age;
-	public void enterClub() {
+	public void enterClub()throws InvalidAgeException {
 		System.out.println("pLease enter your age");
 		age=sc.nextInt();
 		if(age<eligibleAge) {
 			throw new InvalidAgeException("Under age");	
 		}
+		else
+		{
+			System.out.println("Please come...enjoy");
+		}
 	}
 }
 public class Newyearparty {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)throws InvalidAgeException {
+		Newyear n=new Newyear();
+		n.enterClub();
+		System.out.println("Entry granted...enjoy");
+		
+		
 
 	}
 
