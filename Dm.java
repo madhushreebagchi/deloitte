9e93bda25b6b058cf9b28832ae8e4e1c73b13c2b
madhushreebagchi <59157179+madhushreebagchi@users.noreply.github.com>
2019-12-31 03:13:52 -0800

    Add files via upload

--- Dm.java
+++ Dm.java
@@ -1,19 +1,20 @@
 package inheritance;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 public class Dm {
 
 	public static void main(String[] args) {
-		List names=new ArrayList();
+		Set names=new HashSet();
 		names.add("Hiyanshu");
 		names.add("Anthony");
 		names.add("Swami");
 		names.add("Swami");
 System.out.println(names);
-names.add(2,"Reddy");
+names.add("Reddy");
 System.out.println(names);
 names.remove("Swami");
 System.out.println(names.isEmpty());
 System.out.println(names.size());
+
 	}
 }
