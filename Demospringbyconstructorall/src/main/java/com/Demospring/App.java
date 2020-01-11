1e1064636a5572861d53defc9bdbe138bd33450b
madhushreebagchi <59157179+madhushreebagchi@users.noreply.github.com>
2020-01-10 23:15:53 -0800

    Add files via upload

---
+++ Demospringbyconstructorall/src/main/java/com/Demospring/App.java
@@ -0,0 +1,15 @@
+package com.Demospring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Customer customer=new Customer();
+    	customer.setCustomerName("Abhishek");
+        System.out.println( customer );
+    }
+}
