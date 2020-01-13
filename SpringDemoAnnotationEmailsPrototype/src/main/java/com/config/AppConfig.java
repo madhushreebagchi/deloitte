f841cd1ffeef444e444b2ee28d957eaaf4c7e822
madhushreebagchi <59157179+madhushreebagchi@users.noreply.github.com>
2020-01-12 22:18:09 -0800

    Add files via upload

---
+++ SpringDemoAnnotationEmailsPrototype/src/main/java/com/config/AppConfig.java
@@ -0,0 +1,58 @@
+package com.config;
+
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import com.Demospring.Body;
+import com.Demospring.Email;
+import com.Demospring.From;
+import com.Demospring.Subject;
+import com.Demospring.To;
+
+
+
+@Configuration
+public class AppConfig 
+{
+	@Bean
+	@Scope(value="prototype")
+	public Email getEmail()
+	{
+		return new Email();
+	}
+	
+	@Bean
+	public To getTo()
+	{
+		return new To();
+		
+	}
+
+	@Bean
+	public From getFrom()
+	{
+		return new From();
+		
+	}
+	
+
+	@Bean
+	public Subject getSubject()
+	{
+		return new Subject();
+		
+	}
+	
+
+	@Bean
+	public Body toBody()
+	{
+		return new Body();
+		
+	}
+	
+}
---
+++ SpringDemoCustomerContactDetailsCardsPrototype/src/main/java/com/Demospring/AppSpring.java
@@ -0,0 +1,22 @@
+package com.Demospring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+
+public class AppSpring { 
+
+	public static void main(String[] args) 
+	{
+		Resource resource=new ClassPathResource("beans.xml");
+		BeanFactory factory=new XmlBeanFactory(resource);
+		
+	Customer customer=(Customer) factory.getBean("cust");
+	Customer customer2=(Customer) factory.getBean("cust");
+    System.out.println(customer);
+    System.out.println(customer2);
+	}
+
+}
