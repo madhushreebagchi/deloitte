6ad12c8085e3aaa550570eccb9708f05ae36de76
madhushreebagchi <59157179+madhushreebagchi@users.noreply.github.com>
2020-01-13 01:49:21 -0800

    Add files via upload

---
+++ SpringMVCDemo/src/com/deloitte/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.deloitte.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+@RequestMapping("/ahmed")	
+public String gg()
+{
+	return "ghosia";
+}
+@RequestMapping("/customer")	
+public String cc()
+{
+	return "customer";
+}
+@RequestMapping("/employee")	
+public String ee()
+{
+	return "employee";
+}
+@RequestMapping("/product")	
+public String pp()
+{
+	return "product";
+}
+@RequestMapping("/guest")	
+public String ff()
+{
+	return "guest";
+}
+}
